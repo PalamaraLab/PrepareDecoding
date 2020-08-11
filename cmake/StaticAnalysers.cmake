90d69dbf8b62b7bcce1f98607044ab6ba5cf6570
Fergus Cooper <fcooper8472@gmail.com>
2020-08-11 18:07:04 +0100

    Install clang tidy

--- cmake/StaticAnalysers.cmake
+++ cmake/StaticAnalysers.cmake
@@ -5,7 +5,6 @@
 
 option(ENABLE_CPPCHECK "Enable static analysis with cppcheck" OFF)
 option(ENABLE_CLANG_TIDY "Enable static analysis with clang-tidy" OFF)
-option(ENABLE_INCLUDE_WHAT_YOU_USE "Enable static analysis with include-what-you-use" OFF)
 
 if(ENABLE_CPPCHECK)
   find_program(CPPCHECK cppcheck)
@@ -18,7 +17,6 @@ if(ENABLE_CPPCHECK)
         --enable=all
         --inconclusive)
     message(STATUS "Using cppcheck ${CPPCHECK}")
-    message(-i${CMAKE_SOURCE_DIR}/test/)
   else()
     message(SEND_ERROR "cppcheck requested but executable not found")
   endif()
