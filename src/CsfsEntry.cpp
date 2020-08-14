6291697eff8794af539295ce1e632d2c55cc5c47
Fergus Cooper <fcooper8472@gmail.com>
2020-08-14 18:03:10 +0100

    Commit all changes from laptop

---
+++ src/CsfsEntry.cpp
@@ -0,0 +1,35 @@
+// This file is part of https://github.com/PalamaraLab/PrepareDecoding which is released under the GPL-3.0 license.
+// See accompanying LICENSE and COPYING for copyright notice and full details.
+
+#include "CsfsEntry.hpp"
+
+#include <Eigen/Core>
+#include <fmt/core.h>
+#include <fmt/ostream.h>
+
+#include <stdexcept>
+
+namespace asmc {
+
+// using array_t = Eigen::ArrayXd;
+// using mat_t = Eigen::MatrixXd;
+
+CsfsEntry::CsfsEntry(array_t timeVector, array_t sizeVector, double mu, double from, double to, int samples, mat_t csfs)
+    : mMu(mu), mFrom(from), mTo(to), mSamples(samples), mTimeVector(std::move(timeVector)),
+      mSizeVector(std::move(sizeVector)), mCsfs(std::move(csfs)) {
+
+  if (mTimeVector.size() != mSizeVector.size() || mCsfs.rows() != 3 || mCsfs.cols() != mSamples - 1 || mFrom >= mTo) {
+
+    const std::string errorMessage =
+        fmt::format("Time vector:\n{}\nSize vector:\n{}\nCSFS:\n{}\nFrom: {} To: {}\nMalformed CSFS entry.",
+                    mTimeVector.transpose(), mSizeVector.transpose(), mCsfs, mFrom, mTo);
+    throw std::runtime_error(errorMessage);
+  }
+}
+
+std::string CsfsEntry::toString() {
+  return fmt::format("Time:\t{}\nSize:\t{}\nMu:\t{}\nSamples:\t{}\nInterval:\t{}\t{}\n{}\n", mTimeVector.transpose(),
+                     mSizeVector.transpose(), mMu, mSamples, mFrom, mTo, mCsfs);
+}
+
+} // namespace asmc
--- src/CsfsEntry.hpp
+++ src/CsfsEntry.hpp
@@ -4,16 +4,14 @@
 #ifndef PREPAREDECODING_CSFSENTRY_HPP
 #define PREPAREDECODING_CSFSENTRY_HPP
 
-#include <Eigen/Core>
+#include "EigenTypes.hpp"
+
 #include <fmt/core.h>
 #include <fmt/ostream.h>
 
 #include <stdexcept>
 
 namespace asmc {
-
-using array_t = Eigen::ArrayXd;
-using mat_t = Eigen::MatrixXd;
 
 class CsfsEntry {
 
@@ -30,23 +28,9 @@ private:
   mat_t mCsfs = {};
 
 public:
-  CsfsEntry(array_t timeVector, array_t sizeVector, double mu, double from, double to, int samples, mat_t csfs)
-      : mMu(mu), mFrom(from), mTo(to), mSamples(samples), mTimeVector(std::move(timeVector)),
-        mSizeVector(std::move(sizeVector)), mCsfs(std::move(csfs)) {
+  CsfsEntry(array_t timeVector, array_t sizeVector, double mu, double from, double to, int samples, mat_t csfs);
 
-    if (mTimeVector.size() != mSizeVector.size() || mCsfs.rows() != 3 || mCsfs.cols() != mSamples - 1 || mFrom >= mTo) {
-
-      const std::string errorMessage =
-          fmt::format("Time vector:\n{}\nSize vector:\n{}\nCSFS:\n{}\nFrom: {} To: {}\nMalformed CSFS entry.",
-                      mTimeVector.transpose(), mSizeVector.transpose(), mCsfs, mFrom, mTo);
-      throw std::runtime_error(errorMessage);
-    }
-  }
-
-  std::string toString() {
-    return fmt::format("Time:\t{}\nSize:\t{}\nMu:\t{}\nSamples:\t{}\nInterval:\t{}\t{}\n{}\n", mTimeVector.transpose(),
-                       mSizeVector.transpose(), mMu, mSamples, mFrom, mTo, mCsfs);
-  }
+  std::string toString();
 };
 
 } // namespace asmc
