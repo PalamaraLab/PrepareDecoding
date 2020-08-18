467a8b35c15bf116b3d9fb4c0a99cbcb0d6f86b1
Fergus Cooper <fcooper8472@gmail.com>
2020-08-18 01:03:00 +0100

    Check hypergeometric pmf sums to one

--- test/TestUtils.cpp
+++ test/TestUtils.cpp
@@ -7,13 +7,28 @@
 
 namespace asmc {
 
-TEST_CASE("Util: hypergeometric PMF", "[Utils]") {
+TEST_CASE("Util: hypergeometric PMF specific values", "[Utils]") {
 
   Catch::StringMaker<double>::precision = 18;
 
   CHECK(hypergeometricPmf(10'000, 4'270, 300, 87) == Approx(2.209102501827205049068452429E-7).epsilon(1e-9));
   CHECK(hypergeometricPmf(10'000, 4'270, 300, 128) == Approx(0.047240111763330826329736938358).epsilon(1e-9));
+}
 
+TEST_CASE("Util: hypergeometric PMF sums to one", "[Utils]") {
+
+  Catch::StringMaker<double>::precision = 18;
+
+  const int populationSize = 12'345;
+  const int numberOfSuccesses = 6'789;
+  const int sampleSize = 300;
+
+  double total_probability = 0.0;
+  for (int observedSuccesses = 0; observedSuccesses < sampleSize; ++observedSuccesses) {
+    total_probability += hypergeometricPmf(populationSize, numberOfSuccesses, sampleSize, observedSuccesses);
+  }
+
+  CHECK(total_probability == Approx(1.0).epsilon(1e-9));
 }
 
 } // namespace asmc
