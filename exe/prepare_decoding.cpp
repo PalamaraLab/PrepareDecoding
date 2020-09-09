7f3356eb56146e89f0130b40a17f002919a10e7c
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-09-09 12:28:02 +0000

    Add command line options
    cxxopts does not support multi character short options, changed
    mu -> m, qCoal -> q. It also does not support mutually exclusive
    option groups, will add validation later.

--- exe/prepare_decoding.cpp
+++ exe/prepare_decoding.cpp
@@ -6,18 +6,35 @@
 
 int main(int argc, char* argv[]) {
 
-  cxxopts::Options options("MyProgram", "One line description of MyProgram");
+  cxxopts::Options options("ASMCprepareDecoding",
+          "Precompute decoding quantities for ASMC");
 
   // clang-format off
   options.add_options()
-          ("i,integer", "Int param", cxxopts::value<int>()->default_value("-1"))
-          ("f,file", "File name", cxxopts::value<std::string>()->default_value("/some/file"))
-          ("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"));
-  // clang-format on
-
-  auto result = options.parse(argc, argv);
-
-  fmt::print("Option i: {}\n", result["i"].as<int>());
-  fmt::print("Option f: {}\n", result["f"].as<std::string>());
-  fmt::print("Option v: {}\n", result["v"].as<bool>());
+      ("D,demography",
+       "File with demographic model. If not specified, the default EU model will be used.",
+       cxxopts::value<std::string>()->default_value(""))
+      ("C,CSFS",
+       "File with precomputed CSFS. If not specified this program will try to compute it internally.",
+       cxxopts::value<std::string>()->default_value(""))
+      ("n,samples",
+       "Number of total samples to be used for emission probability. If not specified "
+       "will use min(300, sample size)",
+       cxxopts::value<int>()->default_value("0"))
+      ("m,mut", "Mutation rate assumed by demographic model",
+       cxxopts::value<double>()->default_value("1.65e-8"))
+      ("d,discretization", "File with vector of time discretization intervals",
+       cxxopts::value<std::string>())
+      ("q,coalescentQuantiles", "Desired number of discretization intervals "
+       " (quantiles from the pairwise coalescent distribution)",
+       cxxopts::value<std::string>())
+      ("o,OutputFileRoot", "Output file root.",
+       cxxopts::value<std::string>())
+      ("f,fileRoot", "Root for name of hap/samples files from which allele "
+                     " frequencies for array data should be read.",
+       cxxopts::value<std::string>())
+      ("F,freqFile", "Plink .frq file containing minor allele frequencies for array data.",
+       cxxopts::value<std::string>())
+      ;
+  options.parse(argc, argv);
 }
