19ca4add0d3971a460578fd3deb233ead93b30bb
Fergus Cooper <fcooper8472@gmail.com>
2021-04-23 11:53:19 +0100

    Add infrastructure for installing the C++ module

---
+++ cmake/AsmcPrepareDecodingConfig.cmake
@@ -0,0 +1,9 @@
+include(CMakeFindDependencyMacro)
+
+find_dependency(cereal)
+find_dependency(cxxopts)
+find_dependency(eigen3)
+find_dependency(fmt)
+find_dependency(zlib)
+
+include(${CMAKE_CURRENT_LIST_DIR}/AsmcDataModule_Runtime.cmake)
